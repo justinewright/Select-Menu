2f2649280f481e99e196aed57cd61a55f740526b
justine wright <wright.justine21@gmail.com>
2021-11-04 20:28:57 +0200

    Initial Commit

---
+++ Select-Menu/Select_Menu.h
@@ -0,0 +1,18 @@
+//
+//  Select_Menu.h
+//  Select-Menu
+//
+//  Created by Justine Wright on 2021/11/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Select_Menu.
+FOUNDATION_EXPORT double Select_MenuVersionNumber;
+
+//! Project version string for Select_Menu.
+FOUNDATION_EXPORT const unsigned char Select_MenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Select_Menu/PublicHeader.h>
+
+
